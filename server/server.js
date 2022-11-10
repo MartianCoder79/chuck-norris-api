44b3739ffb9a6171714ed2df9464368dabd75029
Manoj Singh <manojsingh@appliedtechnology.se>
2022-11-10 17:56:27 +0100

    can fetch one joke

--- server/server.js
+++ server/server.js
@@ -1,76 +1,54 @@
 // const axios = require("axios");
-const express = require('express');
+import express from 'express';
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import cors from 'cors';
+import bodyParser from 'body-parser';
 // const { request } = require('express');
-const https = require('https');
+import https from 'https';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search',
-//   params: {query: 'food'},
-//   headers: {
-//     accept: 'application/json',
-//     'x-rapidapi-key': 'cJvLRNK0GfdM9WSMbQe3inU7REn8JVy5',
-//     'x-rapidapi-host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
-
-//const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=9bcfb7c4' ;
-// const jokesUrl = 'https://official-joke-api.appspot.com/random_joke' ;
-//const nasaUrl= 'https://api.nasa.gov/planetary/apod?api_key=ehSkrNQZQ2ItdhNJMmucjOoDuD93r7nvD4hDxmwg'
-//const cardsUrl = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
  const chuckUrl = 'https://api.chucknorris.io/jokes/random';
+ const chuckCategories = 'https://api.chucknorris.io/jokes/categories';
 
-let data = '';
+let joke = '';
+let categories = '';
 
+// gets jokes from chuck norris api
 https.get(chuckUrl, (resp) => {
-  // A chunk of data has been received.
   resp.on('data', (chunk) => {
-    data += chunk;
+    joke += chunk;
   });
-  // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    // resp.send(JSON.parse(data))
-    
-    console.log(JSON.parse(data));
+    console.log(JSON.parse(joke));
+  });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+app.get("/random", (req, res) => {
+  res.json( joke);
+})
+
+//gets categories of jokes from the api
+https.get(chuckCategories, (resp) => {
+  resp.on('data', (chunk) => {
+    categories += chunk;
+  });
+  resp.on('end', () => {
+    console.log(JSON.parse(categories));
   });
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
 
-app.get("/users", (req, res) => {
-  //res.json({ "users": ["aaaa", "bbbb", "cccc" ]});
-  res.json( data);
-  //console.log('👿',JSON.parse(data))
+app.get("/random", (req, res) => {
+  res.json(categories);
 })
-
-
-// app.get('/api/movies', function(req, res) {
-//   req(url);
-// })
-
-
-// router.get('/api/omdb', function(request, response, next) {
-//   request({
-//     uri: url
-//   }).pipe(response);
-// });
-
-// app.get('/api/movies', (req, res) => {
-//   request.get
-// })
+app.get("/categories", (req, res) => {
+  res.json(categories);
+})
 
 const port = 5001;
 
